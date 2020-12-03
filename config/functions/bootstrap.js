a89afff8050ccdaee5c91a765f79bb62245315da
ramprits <ramprits-sdei@gmail.com>
2020-12-03 15:39:05 +0530

    adding data

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,13 +1,16 @@
-'use strict';
+"use strict";
+const products = require("../../product.json");
 
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- *
- * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
- */
-
-module.exports = () => {};
+module.exports = async () => {
+  if (productData.length < 0) {
+    products.forEach((product) => {
+      strapi.services.product.create({
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        sale_price: product.sale_price,
+        user: 1,
+      });
+    });
+  }
+};
