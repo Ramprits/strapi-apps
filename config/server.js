0252aac1815a34653c8e78bb7c0c217984a910c8
ramprits <ramprits-sdei@gmail.com>
2020-12-03 14:13:15 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e80878aab739d07481b7e0ec762a1817'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2ef956d-680e-4fe3-9b32-261baa157546'
+};
